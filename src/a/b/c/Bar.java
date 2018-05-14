1bd44aaf7db130ebc70b2db865dd476efc1d95ce
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-05-14 18:46:49 +0300

    add Bar.java

---
+++ src/a/b/c/Bar.java
@@ -0,0 +1,10 @@
+package a.b.c;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 5/14/2018.
+ * Project: r11
+ * *******************************
+ */
+public class Bar {
+}

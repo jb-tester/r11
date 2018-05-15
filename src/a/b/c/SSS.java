81880a94b8845ad9c43ebe574edb054af63e4109
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-05-15 11:35:03 +0300

    multiple changes - 2

---
+++ src/a/b/c/SSS.java
@@ -0,0 +1,18 @@
+package a.b.c;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 5/15/2018.
+ * Project: multiple_github_repos
+ * *******************************
+ */
+public class SSS {
+    private static SSS ourInstance = new SSS();
+
+    public static SSS getInstance() {
+        return ourInstance;
+    }
+
+    private SSS() {
+    }
+}

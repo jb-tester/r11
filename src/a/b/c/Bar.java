5b4ce969a8fb5828a2e0b1f86d749a02c7b58757
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-05-15 14:58:26 +0300

    Bar.java edit from clone again

--- src/a/b/c/Bar.java
+++ src/a/b/c/Bar.java
@@ -4,11 +4,11 @@ package a.b.c;
  * *******************************
  * Created by Irina.Petrovskaya on 5/14/2018.
  * Project: r11
- * fffffff
+ * ff
  * *******************************
  */
 public class Bar {
-    // here
-    // ddd
-    // dddddd
+    //
+    //
+    //
 }

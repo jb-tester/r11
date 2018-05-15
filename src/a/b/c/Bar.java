47ccf238d748fb86f5e9f4f0bc0b86f751a95804
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-05-15 14:59:31 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/a/b/c/Bar.java
    
    Merge remote-tracking branch 'origin/master'
    
    # Conflicts:
    #	src/a/b/c/Foo.java

--- src/a/b/c/Bar.java
+++ src/a/b/c/Bar.java
@@ -4,11 +4,16 @@ package a.b.c;
  * *******************************
  * Created by Irina.Petrovskaya on 5/14/2018.
  * Project: r11
- * ff
+ *
+ *
+ * fffffff
  * *******************************
  */
 public class Bar {
-    //
-    //
-    //
+    // here
+    // ch1
+    // nnnn
+    // ch1 fff
+    // ddd
+    // dddddd
 }

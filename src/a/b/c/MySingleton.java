4ba8c65a2bdc0f5c0955eac0230f546e1efadd62
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-05-15 20:02:54 +0300

    MySingleton.java

--- src/a/b/c/MySingleton.java
+++ src/a/b/c/MySingleton.java
@@ -4,9 +4,6 @@ package a.b.c;
  * *******************************
  * Created by Irina.Petrovskaya on 5/15/2018.
  * Project: multiple_github_repos
- *
- *
- *
  * *******************************
  */
 public class MySingleton {

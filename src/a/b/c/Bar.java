606aec12db6263844330b4dad2063ef31ba4a20e
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-05-15 17:10:26 +0300

    dummy changes

--- src/a/b/c/Bar.java
+++ src/a/b/c/Bar.java
@@ -4,7 +4,7 @@ package a.b.c;
  * *******************************
  * Created by Irina.Petrovskaya on 5/14/2018.
  * Project: r11
- *
+ * *******************************
  * *******************************
  */
 public class Bar {

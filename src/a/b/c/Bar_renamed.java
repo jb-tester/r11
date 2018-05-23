b286ef01af66b9e493951e38c61e94e88161f9f9
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-05-23 14:27:42 +0300

    attempt to rebase

---
+++ src/a/b/c/Bar_renamed.java
@@ -0,0 +1,20 @@
+package a.b.c;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 5/14/2018.
+ * Project: r11
+  * ...................
+  * ttttttttttttttttttttttttt
+ * *******************************
+ */
+public class Bar_renamed {
+    // dkdkk
+    // ssh1
+    // ssh1
+    // ssh1
+    // ssh1
+    // clone
+    // clone
+    // clone
+}
